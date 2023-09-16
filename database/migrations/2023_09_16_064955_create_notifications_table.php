71756d2280d81c6a2139925b57e12b92ebf762bf
Amol <djamolgroup@gmail.com>
2023-09-16 03:07:37 -0400

    User and Notification models - relationships and define notification table structure in migration

--- database/migrations/2023_09_16_064955_create_notifications_table.php
+++ database/migrations/2023_09_16_064955_create_notifications_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->string('type');
+            $table->unsignedBigInteger('notifiable_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('short_text');
+            $table->timestamp('expiration');
+            $table->string('destination');
+            $table->timestamp('read_at')->nullable();
+            $table->string('notifiable_type');
+            $table->boolean('read')->default(false);
             $table->timestamps();
         });
     }
